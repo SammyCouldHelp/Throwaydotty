ef206d4c4055aaba0ce515a52074a6d6fcc7db53
Samuel Dibiah <samuelchimdidibiah@gmail.com>
2024-10-26 05:54:59 +0100

    Updated the nav bar

--- scripts/script.js
+++ scripts/script.js
@@ -36,3 +36,11 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+// nav
+
+function toggleMenu() {
+  const menu = document.querySelector (".menu-links");
+  const icon = document.querySelector (".hamburger-icon");
+  menu.classList.toggle ("open")
+  icon.classList.toggle ("open")
+}
